01091cf80e6573bbad2c3d76601296cfd10dbd58
KIYO <moodyz83@yahoo.co.jp>
2022-06-05 21:28:36 +0900

    revised app_adding DL func

--- sqlapp.py
+++ sqlapp.py
@@ -7,8 +7,10 @@ from sqlalchemy import Column, Integer, Float, String, DateTime, Boolean, Foreig
 from sqlalchemy import desc #降順ソート
 
 import pandas as pd
+import os, glob
 
 DB_file = 'testDB.db'
+csv_file = 'users.csv'
 engine = create_engine('sqlite:///' + DB_file, echo=False) #DB接続の設定
 
 Session = scoped_session(
@@ -37,17 +39,54 @@ init_DB()
 
 st.title('SqliteをStreamlitで使ってみる')
 
+def read_users(engine):
+    df = pd.read_sql('SELECT * FROM users', engine)
+    return df
+
+def update_csv(engine):
+    df = read_users(engine)
+    df.to_csv(csv_file, index=False)
+
 name = st.text_input('名前を入力してください')
 if st.button('名前を登録'):
     if name != '':
         user = User(name)
         Session.add(user)
         Session.commit()
+        update_csv(engine)
     else:
         st.write('名前を入力してください。')
 
+
 st.markdown('***')   
 if st.button('名前一覧を表示'):
-    df = pd.read_sql('select * from users', engine)
+    df = read_users(engine)
     st.dataframe(df)
-    
+
+st.markdown('***')   
+filetype = st.selectbox('ファイルタイプを選択してください', ['csv', 'sqlite'])
+if filetype == 'csv':
+    df = read_users(engine)
+    st.download_button(label=f'{filetype}形式でデータをダウンロード',
+                    data = df.to_csv(index=False).encode('utf-8'),
+                    file_name = csv_file,
+                    mime = 'text/csv')
+elif filetype == 'sqlite':
+    with open(DB_file, "rb") as fp:
+        btn = st.download_button(
+            label=f'{filetype}形式でデータをダウンロード',
+            data=fp,
+            file_name=DB_file,
+            mime="application/octet-stream" #https://discuss.streamlit.io/t/download-sqlite-db-file/18747/2
+        )
+else:
+    st.write('CSVかsqliteで選択してください。')
+
+
+st.markdown('***')   
+st.markdown('### 参考用')
+st.write('os.getcwd()：',os.getcwd())
+st.write("glob.glob('*')：",glob.glob('*'))
+st.write('os.name：', os.name) #OS名称：Windows->'nt', Linux->'posix'
+st.write('os.getlogin():', os.getlogin())#ログインユーザー名を取得
+st.write('os.cpu_count() :', os.cpu_count()) #CPU数を取得
